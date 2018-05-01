eff7d5c1f00ad5669ed729892995b71342dfdb22
Pedro Kalil <kalilgamer0@gmail.com>
2018-05-01 15:35:18 +0000

    audio: dont use pathmap

--- Android.mk
+++ Android.mk
@@ -2,26 +2,26 @@
 
 #ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 apq8098_latv,$(TARGET_BOARD_PLATFORM)),)
 
-MY_LOCAL_PATH := $(call my-dir)
+QCOM_AUDIO_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(MY_LOCAL_PATH)/legacy/Android.mk
+include $(QCOM_AUDIO_ROOT)/legacy/Android.mk
 else
 ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
-include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
+include $(QCOM_AUDIO_ROOT)/hal_mpq/Android.mk
 else
-include $(MY_LOCAL_PATH)/hal/Android.mk
+include $(QCOM_AUDIO_ROOT)/hal/Android.mk
 endif
-include $(MY_LOCAL_PATH)/voice_processing/Android.mk
-include $(MY_LOCAL_PATH)/mm-audio/Android.mk
-include $(MY_LOCAL_PATH)/policy_hal/Android.mk
-include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/post_proc/Android.mk
-include $(MY_LOCAL_PATH)/qahw_api/Android.mk
+include $(QCOM_AUDIO_ROOT)/voice_processing/Android.mk
+include $(QCOM_AUDIO_ROOT)/mm-audio/Android.mk
+include $(QCOM_AUDIO_ROOT)/policy_hal/Android.mk
+include $(QCOM_AUDIO_ROOT)/visualizer/Android.mk
+include $(QCOM_AUDIO_ROOT)/post_proc/Android.mk
+include $(QCOM_AUDIO_ROOT)/qahw_api/Android.mk
 endif
 
 ifeq ($(USE_LEGACY_AUDIO_DAEMON), true)
-include $(MY_LOCAL_PATH)/audiod/Android.mk
+include $(QCOM_AUDIO_ROOT)/audiod/Android.mk
 endif
 
 #endif
